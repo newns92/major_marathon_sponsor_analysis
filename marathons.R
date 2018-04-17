03ca638b6757bec300832c2cb068c5ad81e3fdd3
Steve Newns <s.newns92@gmail.com>
2018-04-17 19:55:56 -0400

    edit splitting code, need to add logic to compare even vs. odd nchar() results

--- marathons.R
+++ marathons.R
@@ -26,12 +26,12 @@ chicago <- read_html('https://en.wikipedia.org/wiki/List_of_winners_of_the_Chica
 substrRight <- function(x, n){
   substr(x, nchar(x)-n+1, nchar(x))
 }
-substrleft <- function(x, n){
-  substr(x, nchar(x)-n+1, nchar(x))
+substrLeft <- function(x, n){
+  substr(x, 0, n)
 }
 
 #**********************************************************************************************/
-
+  
 tokyo_men <- tokyo[[2]][,1:4] %>%
   rename(winner="Men's winner",country=Country,"time"="Time (m:s)",year=Year) %>%
   mutate(year = substr(year,0,4),gender="Male")
@@ -74,11 +74,11 @@ london_women <- london[[2]] %>%
 london_full <- bind_rows(london_men,london_women) %>%
   mutate(marathon="London")
 
-boston_men <- boston[[1]] %>%
+boston_men <- boston[[2]] %>%
   rename(winner=Athlete,country="Country/State","time"="Time",year=Year) %>%
   mutate(gender="Male") %>%
   select(year,winner,gender,country,time,-Notes)
-boston_women <- boston[[2]] %>%
+boston_women <- boston[[3]] %>%
   rename(winner=Athlete,country="Country/State","time"="Time",year=Year) %>%
   mutate(gender="Female") %>%
   select(year,winner,gender,country,time,-Notes)
@@ -97,31 +97,43 @@ chicago_full <- bind_rows(chicago_men,chicago_women) %>%
   mutate(marathon="Chicago")
 
 # south korea and 17 18 not doubled?
-#boston_full$new_winner = ""
+boston_full$winner[74] <- "Hill, Ron !Ron Hill"
 for (i in 1:(nrow(boston_full))) {
   boston_full$winner[i] <- if_else(boston_full$country[i] == "South Korea",boston_full$winner[i],
          if_else(boston_full$year[i] %in% c(2017,2018) & boston_full$gender[i] == "Male",boston_full$winner[i],
-                substrleft(boston_full$winner[i], nchar(boston_full$winner[i])/2 - 1)))}
-
+                #substrleft(boston_full$winner[i], nchar(boston_full$winner[i])/2 - 1)))}
+                substrLeft(boston_full$winner[i],
+                           if_else(substrRight(boston_full$winner[i],5)=="(Tie)",
+                                   round(nchar(substrLeft(boston_full$winner[i],
+                                                          nchar(boston_full$winner[i])-6))/2)+1,
+                                   round(nchar(boston_full$winner[i])/2)+1))))
+}
+boston_full$winner[22] <- "Camp Devens relay team[3]"
+  
 #tail(boston_full)
-#tail(subset(boston_full,gender=="Male")
-
+#tail(subset(boston_full,gender=="Male"))
+  
 head(london_full)
 for (i in 1:(nrow(london_full))) {
-  london_full$winner[i] <- substrleft(london_full$winner[i], nchar(london_full$winner[i])/2 - 1)
+  london_full$winner[i] <- substrLeft(london_full$winner[i],
+                                      if_else(substrRight(london_full$winner[i],5)=="(Tie)",
+                                              round(nchar(substrLeft(london_full$winner[i],
+                                                                     nchar(london_full$winner[i])-6))/2)+1,
+                                              round(nchar(london_full$winner[i])/2)+1))
 }
-head(london_full)
-tail(london_full)
-  
+    
+
 for (i in 1:(nrow(berlin_full))) {
-  berlin_full$winner[i] <- substrleft(berlin_full$winner[i], nchar(berlin_full$winner[i])/2 - 1)
+  berlin_full$winner[i] <- substrLeft(berlin_full$winner[i], round(nchar(berlin_full$winner[i])/2) + 1)
 }
+
 head(berlin_full)
 tail(berlin_full)
 
 
 for (i in 1:(nrow(chicago_full))) {
-  chicago_full$winner[i] <- substrleft(chicago_full$winner[i], nchar(chicago_full$winner[i])/2 - 1)
+  chicago_full$winner[i] <- substrLeft(chicago_full$winner[i], round(nchar(chicago_full$winner[i])/2) + 1)
 }
+chicago_full$winner[69] <- "Diță, Constantina"
 head(chicago_full)
 tail(chicago_full)
