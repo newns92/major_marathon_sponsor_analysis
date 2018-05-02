9bbf5f8be70f61d623112ed673717b1389402a0e
Newns <s.newns92@gmail.com>
2018-05-02 16:28:45 -0400

    continue write-up and some viz edits

--- marathons.R
+++ marathons.R
@@ -210,3 +210,4 @@ write.csv(majorMarathons, file = "majorMarathons.csv",row.names=F)
 #74 <- Ron Hill # why a '!'???
 #124,125 <- Bobbi Gibb:  where did middle a come from?
 #135 <- S.Gayle Barron: middle initial?
+
